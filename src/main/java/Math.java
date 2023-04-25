727b40b4057a84c3b9d5e60975ba342cfc0c9591
Abdul Rizwan <abdulrizwan706@gmail.com>
2023-04-26 01:21:58 +0530

    this is it

--- src/main/java/Math.java
+++ src/main/java/Math.java
@@ -1,3 +1,13 @@
 public class Math {
-// write code for Math class here
+    // write code for Math class here
+    // Create a class Math with following requirements
+    // - A class level data member called PI set to 3.14
+    // - A class level method called getCircleArea,
+    //  which takes as input an integer parameter called radius.
+    //   This function should return area of the circle as a double
+    static double pi = 3.14;
+    static double getCircleArea(int radius){
+        return pi*radius*radius;
+    }
+
 }
